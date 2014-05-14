0f56be6964ca628c888f55b2a6bff725e33185d4
Yi Zhang <yzhang.wa@gmail.com>
2014-05-14 10:10:49 +0800

    To bump the version to 0.2

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_ldap_ou_to_group do
   name 'Redmine LDAP Organiztion Unit to Group Plugin'
   author 'Yi Zhang'
   description 'This is a plugin to help sync ldap ou to redmine group'
-  version '0.1'
+  version '0.2'
   requires_redmine :version_or_higher => '2.0.0'
   url 'https://github.com/yzhanginwa/redmine_ldap_ou_to_group'
 end
